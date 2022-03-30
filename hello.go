7a157a5e5c7bf782b34226b5aebba044522ae9b9
mackenziebishop <69914681+mackenziebishop@users.noreply.github.com>
2022-03-30 11:28:49 -0500

    Added hello message

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("This is Dr. Case's Beam Go Hello project!")
+	fmt.Println("Hello, 世界!")
+	fmt.Println("Hello, వరల్డ్!")
+	fmt.Println("Hello, world!")
+	fmt.Println("Too bad PowerShell doesn't recognize runes!")
+}
